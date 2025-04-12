9da76fee48cd83ec102e3045b8d92619af1bad4c
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2025-04-13 01:12:45 +0700

    feat: Add _ViewStart & _ViewImports for each Areas

--- LuxenHotel/LuxenHotel/Data/SeedData.cs
+++ LuxenHotel/LuxenHotel/Data/SeedData.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace LuxenHotel.Data
 {
--- LuxenHotel/LuxenHotel/Program.cs
+++ LuxenHotel/LuxenHotel/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-
 namespace LuxenHotel
 {
     public class Program
